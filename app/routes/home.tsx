58709557d87e2d770e358b9295f3083cef16a83d
nac62116 <cpnash@protonmail.ch>
2024-12-30 17:39:28 +0100

    Starting with a 10x10 static tetris board

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Story Tetris" },
+    { name: "description", content: "TODO: <Story headline>" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const currentBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <div className="grid grid-cols-10 grid-rows-10 place-items-center gap-1">
+        {currentBoard.map((row, rowIndex) =>
+          row.map((cell, cellIndex) => (
+            <div
+              key={`${rowIndex}-${cellIndex}`}
+              className="w-10 h-10 border-2 border-gray-600"
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
