552fbd1396e8a1ab2a1b8263e1ff2ca24d1790f2
nac62116 <cpnash@protonmail.ch>
2025-02-16 13:37:28 +0100

    Update deps to latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
+  build: {
+    target: "es2023",
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
